afdff85c8bc998ec4c78c53d54ec3e5d44541f43
Ultral1sk <jderebanoski@gmail.com>
2020-06-06 21:30:56 +0200

    TypeScript Core Types Boolean and String

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-function add(n1, n2) {
-    return n1 + n2;
+/* Core Type NUMBER */
+// function add( n1 :number, n2 :number ) {
+//       return n1 +  n2;
+// }
+// const number1 = 5;
+// const number2 = 2.8
+// const result = add(number1, number2);
+/* Core Type Boolean + String */
+function add(n1, n2, showResult, phrase) {
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
 }
 var number1 = 5;
 var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
+var printResult = true;
+var resultPhrase = "Result is : ";
+var result = add(number1, number2, printResult, resultPhrase);
--- app.ts
+++ app.ts
@@ -1,10 +1,39 @@
-function add( n1 :number, n2 :number ) {
-      return n1 +  n2;
+/* Core Type NUMBER */
+
+// function add( n1 :number, n2 :number ) {
+//       return n1 +  n2;
+// }
+
+// const number1 = 5;
+// const number2 = 2.8
+
+
+// const result = add(number1, number2);
+
+
+
+
+/* Core Type Boolean + String */
+
+function add( n1 :number, n2 :number, showResult:boolean, phrase : string ) {
+
+      const result = n1 + n2
+
+      if(showResult) {
+        console.log(phrase + result)
+
+      } else {
+            return result
+      }
+
+      
 }
 
-const number1 = 5;
+let number1 : number
+number1 = 5;
+// number1 = "5" <-- throws an error because we specified that we want to store nubmers not strings or etc.
 const number2 = 2.8
+const printResult = true;
+const resultPhrase = `Result is : `
 
-
-const result = add(number1, number2);
-console.log(result)
+const result = add(number1, number2, printResult, resultPhrase);
