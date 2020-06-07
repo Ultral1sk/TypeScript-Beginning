4990d1f4861ef0580c3af6953908a4c9146da7df
Ultral1sk <jderebanoski@gmail.com>
2020-06-07 13:05:01 +0200

    compiler Options ts.config - target & lib keys explained

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 "use strict";
-var user;
-user = "JHovanW";
-console.log(user);
+// how does TTypeScriot know here that the button const has a document object inside
+// why is it not complaining
+const button = document.querySelector('button');
+// the same for the event listener how it recognizes the event listener 
+button.addEventListener('click', () => {
+    console.log(`clicked`);
+});
+// this is the magic of "lib" : [], inside the ts.config
+// in this case when nth is assigned it has some default options 
+// and those default depend on the target or which version of js we've specified inside the target
+// all es6 syntaxx will be supported and also all DOM API's 
+// if we outcoment the lib , we are losing the default settings and we need to specify on our own
--- app.ts
+++ app.ts
@@ -1,5 +1,15 @@
-let user: string;
-user = "JHovanW"
+// how does TTypeScriot know here that the button const has a document object inside
+// why is it not complaining
+const button = document.querySelector('button')!;
+
+// the same for the event listener how it recognizes the event listener 
+button.addEventListener('click', () => {
+      console.log(`clicked`)
+});
 
 
-console.log(user)
+// this is the magic of "lib" : [], inside the ts.config
+// in this case when nth is assigned it has some default options 
+// and those default depend on the target or which version of js we've specified inside the target
+// all es6 syntaxx will be supported and also all DOM API's 
+// if we outcoment the lib , we are losing the default settings and we need to specify on our own
