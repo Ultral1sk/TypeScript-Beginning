4e004ae5648a6ad7bf7db8cba235c273835d6f36
Ultral1sk <jderebanoski@gmail.com>
2020-06-07 12:44:55 +0200

    TypeScript configuration Options and Compiler Options

--- app.js
+++ app.js
@@ -1,12 +1,4 @@
-var userInput;
-var userName;
-userInput = 'jovan';
-if (typeof userInput === 'string') {
-    userName = userInput;
-    console.log(userName);
-}
-// utility function that throws errors
-function generateError(message, code) {
-    throw { message: message, errorCode: code };
-}
-generateError("Error occured with status", 500);
+"use strict";
+var user;
+user = "JHovanW";
+console.log(user);
--- app.ts
+++ app.ts
@@ -1,17 +1,5 @@
-let userInput : unknown;
-let userName: string;
-
-userInput = 'jovan';
+let user: string;
+user = "JHovanW"
 
 
-if( typeof userInput === 'string') {
-      userName = userInput
-      console.log(userName) 
-}
-
-// utility function that throws errors
-function generateError(message : string, code : number) {
-      throw { message : message, errorCode : code }
-}
-
-generateError(`Error occured with status`, 500)
+console.log(user)
