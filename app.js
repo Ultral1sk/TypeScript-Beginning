9bb15941902b3da1b2e62ba50a780b36a69fae2e
Ultral1sk <jderebanoski@gmail.com>
2020-06-07 12:17:53 +0200

    Never Type & TypeScript Basic Finished

--- app.js
+++ app.js
@@ -5,3 +5,8 @@ if (typeof userInput === 'string') {
     userName = userInput;
     console.log(userName);
 }
+// utility function that throws errors
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+generateError("Error occured with status", 500);
--- app.ts
+++ app.ts
@@ -8,3 +8,10 @@ if( typeof userInput === 'string') {
       userName = userInput
       console.log(userName) 
 }
+
+// utility function that throws errors
+function generateError(message : string, code : number) {
+      throw { message : message, errorCode : code }
+}
+
+generateError(`Error occured with status`, 500)
