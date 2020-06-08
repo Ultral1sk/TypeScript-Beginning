a922f5754382e9e7796eb58fc59c6c9dfd328b7b
Ultral1sk <jderebanoski@gmail.com>
2020-06-08 21:18:31 +0200

    Private, Public options inside a class and preventing manipulation from outisde with various different ways so we stay focused by only one way of manipulating the class from outside

--- dist/app.js
+++ dist/app.js
@@ -1,22 +1,27 @@
 "use strict";
-const hobbies = ["pcgames", "programing"];
-const activeHobbies = ["NBA"];
-// console.log([...hobbies, ...activeHobbies])
-const person = {
-    firstName: 'Jovan',
-    age: 30
-};
-// const copiedPerson = {...person}
-// console.log(copiedPerson)
-// accepting random number of arguments inside an array
-// const add = (...numbers : number[]) => {
-//       return numbers.reduce((curResult, curValue) => {
-//             return curResult + curValue
-//       }, 0 )
-// }
-// console.log(add(5,2,6,8))
-// or we can go with the TUPLE option by setting the arguments to fixed lenght and value
-// (...numbers : [number, number, number])
-// const [ hobbyOne, hobbyTwo, ...remainingHobies ] = hobbies
-// console.log(hobbyOne, hobbyTwo)
-const { firstName, age } = person;
+class Department {
+    constructor(n) {
+        this.employeess = [];
+        this.name = n;
+    }
+    describe() {
+        console.log(`Department: ${this.name}`);
+    }
+    addEmployee(employee) {
+        this.employeess.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employeess.length);
+        console.log(this.employeess);
+    }
+}
+const departmendAccounting = new Department(`accounting`);
+departmendAccounting.addEmployee('Max');
+departmendAccounting.addEmployee('Manu');
+// the class is still mutable from outside like this by accesing the property only
+// what we need to do here is just make only one way of doing this, meaning manipulating the class through only one way
+// because we don't want to have sideeffects meaning to update or affect the class unpurposly
+// departmendAccounting.employeess[2] = "jovan"   // <-- we dont want this to be available we ned to make it private
+departmendAccounting.describe();
+departmendAccounting.printEmployeeInformation();
+//# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,34 @@
-const hobbies = ["pcgames", "programing"];
-const activeHobbies = ["NBA"];
+class Department {
+  public name: string; // <-- public is default property to the key and it is also muatble from outside
+  private employeess: string[] = [];
 
-// console.log([...hobbies, ...activeHobbies])
+    constructor( n: string ) {
+      this.name = n;
+      }
 
+    describe(this : Department) {
+          console.log(`Department: ${this.name}`);
+    }
 
-const person = {
-      firstName : 'Jovan',
-      age : 30
+    addEmployee(employee: string) {
+          this.employeess.push(employee);
+    }
+
+    printEmployeeInformation() {
+          console.log(this.employeess.length);
+          console.log(this.employeess);
+    }
 }
 
-// const copiedPerson = {...person}
-// console.log(copiedPerson)
 
+const departmendAccounting = new Department(`accounting`) 
+departmendAccounting.addEmployee('Max')
+departmendAccounting.addEmployee('Manu')
 
+// the class is still mutable from outside like this by accesing the property only
+// what we need to do here is just make only one way of doing this, meaning manipulating the class through only one way
+// because we don't want to have sideeffects meaning to update or affect the class unpurposly
+// departmendAccounting.employeess[2] = "jovan"   // <-- we dont want this to be available we ned to make it private
 
-// accepting random number of arguments inside an array
-// const add = (...numbers : number[]) => {
-//       return numbers.reduce((curResult, curValue) => {
-//             return curResult + curValue
-//       }, 0 )
-// }
-
-
-// console.log(add(5,2,6,8))
-// or we can go with the TUPLE option by setting the arguments to fixed lenght and value
-// (...numbers : [number, number, number])
-
-
-// const [ hobbyOne, hobbyTwo, ...remainingHobies ] = hobbies
-// console.log(hobbyOne, hobbyTwo)
-
-const { firstName, age } = person
+departmendAccounting.describe()
+departmendAccounting.printEmployeeInformation();
