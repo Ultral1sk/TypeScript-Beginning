950912c04f764bbf6aea1492d3d953e6ecc9fd07
Ultral1sk <jderebanoski@gmail.com>
2020-06-08 21:51:02 +0200

    Class inheritance with Constructor and calling super() && accessing the baseclass methods and use another logic inside and make another usage of it

--- dist/app.js
+++ dist/app.js
@@ -3,10 +3,16 @@ class Department {
     constructor(id, name) {
         this.id = id;
         this.name = name;
+        //   private readyonly id: string;
+        //   priovate name : string;
+        //   private is only accessible from inside the class not from outside
+        // but if we still want to be private but still accessible from the other classes instead of PRIVATE we use PROTECTED
         this.employeess = [];
+        // this.id = id;
+        // this.name = n;
     }
     describe() {
-        console.log(`Department: ${this.name} with an id ${this.id}`);
+        //     console.log(`Department: ${this.name} with an id ${this.id}`);
     }
     addEmployee(employee) {
         this.employeess.push(employee);
@@ -16,13 +22,45 @@ class Department {
         console.log(this.employeess);
     }
 }
-const departmendAccounting = new Department('d1', `accounting`);
-departmendAccounting.addEmployee('Max');
-departmendAccounting.addEmployee('Manu');
-// the class is still mutable from outside like this by accesing the property only
-// what we need to do here is just make only one way of doing this, meaning manipulating the class through only one way
-// because we don't want to have sideeffects meaning to update or affect the class unpurposly
-// departmendAccounting.employeess[2] = "jovan"   // <-- we dont want this to be available we ned to make it private
-departmendAccounting.describe();
-departmendAccounting.printEmployeeInformation();
+//even though the class is empty with the EXTENDS keyword we are gonna inherit everythig that class has
+// but when we want to get particular stuff we do it through the constructor and also by calling super() 
+class ITDepartment extends Department {
+    // this is passed to super
+    constructor(id, admins) {
+        super(id, 'IT');
+        this.admins = admins;
+        this.admins = admins;
+    }
+}
+class AccountingDepartment extends Department {
+    constructor(id, reports) {
+        super(id, 'Accounting');
+        this.reports = reports;
+    }
+    addEmployee(name) {
+        if (name === 'Max') {
+            return;
+        }
+        this.employeess.push(name);
+    }
+    addReport(text) {
+        this.reports.push(text);
+    }
+    printReports() {
+        console.log(this.reports);
+    }
+}
+const it = new ITDepartment('d1', ['Jojo']);
+it.addEmployee('Max');
+it.addEmployee('Manu');
+it.describe();
+it.name = 'NEW NAME';
+it.printEmployeeInformation();
+console.log(it);
+const accounting = new AccountingDepartment('d2', []);
+accounting.addEmployee(`Max`);
+accounting.addEmployee(`JovanAdmin`);
+accounting.addReport('Smthing went wrong...');
+accounting.printReports();
+accounting.printEmployeeInformation();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
-class Department {
-  private employeess: string[] = [];
+class Department { 
+//   private readyonly id: string;
+//   priovate name : string;
+//   private is only accessible from inside the class not from outside
+// but if we still want to be private but still accessible from the other classes instead of PRIVATE we use PROTECTED
+  protected employeess: string[] = [];
 
     constructor(private readonly id: string, public name: string )  { //<-- this is a shortcut from the previously written code
-  
+      // this.id = id;
+      // this.name = n;
       }
 
     describe(this : Department) {
-          console.log(`Department: ${this.name} with an id ${this.id}`);
+      //     console.log(`Department: ${this.name} with an id ${this.id}`);
     }
 
     addEmployee(employee: string) {
@@ -19,17 +24,51 @@ class Department {
     }
 }
 
+//even though the class is empty with the EXTENDS keyword we are gonna inherit everythig that class has
+// but when we want to get particular stuff we do it through the constructor and also by calling super() 
+class ITDepartment extends Department {
+      // this is passed to super
+      constructor( id: string, public admins : string[] ) {
+        super(id, 'IT');
+            this.admins = admins;
+      }
+}
 
-const departmendAccounting = new Department('d1',`accounting`) 
-departmendAccounting.addEmployee('Max')
-departmendAccounting.addEmployee('Manu')
+class AccountingDepartment extends Department {
+      constructor( id: string, private reports: string[] ){
+            super(id, 'Accounting');
+      }
 
-// the class is still mutable from outside like this by accesing the property only
-// what we need to do here is just make only one way of doing this, meaning manipulating the class through only one way
-// because we don't want to have sideeffects meaning to update or affect the class unpurposly
-// departmendAccounting.employeess[2] = "jovan"   // <-- we dont want this to be available we ned to make it private
+      addEmployee(name : string) {
+            if(name === 'Max') {
+                  return;
+            }
+            this.employeess.push(name)
+      }
 
-departmendAccounting.describe()
-departmendAccounting.printEmployeeInformation();
+      addReport( text: string) {
+            this.reports.push(text);
+      }
+
+      printReports() {
+            console.log(this.reports)
+      }
+}
+
+const it = new ITDepartment('d1', ['Jojo']) 
+it.addEmployee('Max')
+it.addEmployee('Manu')
+
+it.describe()
+it.name = 'NEW NAME'
+it.printEmployeeInformation();
+console.log(it)
 
 
+const accounting = new AccountingDepartment('d2', []);
+accounting.addEmployee(`Max`);
+accounting.addEmployee(`JovanAdmin`);
+
+accounting.addReport('Smthing went wrong...');
+accounting.printReports()
+accounting.printEmployeeInformation()
