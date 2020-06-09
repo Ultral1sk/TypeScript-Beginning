f8efb92b8c3e25f7a7162f2c2f211aca4ea1a806
Ultral1sk <jderebanoski@gmail.com>
2020-06-10 01:53:21 +0200

    Ready only int, extending int, optional params int, function types int

--- dist/app.js
+++ dist/app.js
@@ -1,13 +1,24 @@
 "use strict";
+let add;
+add = (n1, n2) => {
+    return n1 + n2;
+};
 class Person {
     constructor(n) {
-        this.name = n;
+        if (n) {
+            this.name = n;
+        }
     }
     greet(phrase) {
-        console.log(phrase + ' ' + this.name);
+        if (this.name) {
+            console.log(phrase + ' ' + this.name);
+        }
+        else {
+            console.log(`Hi`);
+        }
     }
 }
 let user1;
-user1 = new Person('Max');
+user1 = new Person();
 user1.greet('Hi there I-am');
 //# sourceMappingURL=app.js.map
