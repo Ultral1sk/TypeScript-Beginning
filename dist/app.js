a6fd9eb4ec8bc4be0f1e01c4b90617c9e5fd5333
Ultral1sk <jderebanoski@gmail.com>
2020-06-10 00:58:58 +0200

    TypeScript interfaces

--- dist/app.js
+++ dist/app.js
@@ -1,97 +1,11 @@
 "use strict";
-class Department {
-    constructor(id, name) {
-        this.id = id;
-        this.name = name;
-        this.employeess = [];
-        // this.id = id;
-        // this.name = n;
-        // console.log(Department.fiscalYear) <== This is not accessible from the constructor or the newlycreate instances
-        // but there is a way if we want to acces it by calling the name of the Class in this case like (Department.fiscalYear)
+let user1;
+user1 = {
+    name: 'Max',
+    age: 30,
+    greet(phrase) {
+        console.log(phrase + ' ' + this.name);
     }
-    static createEmployee(name) {
-        return { name: name };
-    }
-    addEmployee(employee) {
-        this.employeess.push(employee);
-    }
-    printEmployeeInformation() {
-        console.log(this.employeess.length);
-        console.log(this.employeess);
-    }
-}
-//   private readyonly id: string;
-//   priovate name : string;
-//   private is only accessible from inside the class not from outside
-// but if we still want to be private but still accessible from the other classes instead of PRIVATE we use PROTECTED
-Department.fiscalYear = 2020;
-//even though the class is empty with the EXTENDS keyword we are gonna inherit everythig that class has
-// but when we want to get particular stuff we do it through the constructor and also by calling super() 
-class ITDepartment extends Department {
-    // this is passed to super
-    constructor(id, admins) {
-        super(id, 'IT');
-        this.admins = admins;
-        this.admins = admins;
-    }
-    describe() {
-        console.log(`IT DEPARTMENT - ID ${this.id}`);
-    }
-}
-class AccountingDepartment extends Department {
-    constructor(id, reports) {
-        super(id, 'Accounting');
-        this.reports = reports;
-        this.lastReport = reports[0];
-    }
-    // this is used if we want to get a property of the class encapsulate it and add extra logic to it
-    // in this case we take the private lastReport and we want to add extra logic whenever we acces it
-    get mostRecentReport() {
-        if (this.lastReport) {
-            return this.lastReport;
-        }
-        throw new Error(`No report found.`);
-    }
-    // with this we do the same but instead of getting a property we can set a new property encapsulate it and add extralogic to it
-    set mostRecentReport(value) {
-        if (!value) {
-            throw new Error(`Plase pass in a valid value`);
-        }
-        this.addReport(value);
-    }
-    describe() {
-        console.log(`IT ACCOUNTING DEPARTNEBT - ID ${this.id}`);
-    }
-    addEmployee(name) {
-        if (name === 'Max') {
-            return;
-        }
-        this.employeess.push(name);
-    }
-    addReport(text) {
-        this.reports.push(text);
-        this.lastReport = text;
-    }
-    printReports() {
-        console.log(this.reports);
-    }
-}
-const employee1 = Department.createEmployee('joko');
-console.log(employee1, Department.fiscalYear);
-const it = new ITDepartment('d1', ['Jojo']);
-it.addEmployee('Max');
-it.addEmployee('Manu');
-it.describe();
-it.name = 'NEW NAME';
-it.printEmployeeInformation();
-console.log(it);
-const accounting = new AccountingDepartment('d2', []);
-accounting.mostRecentReport = `Your end report please`; // <=== use set(setter)
-console.log(accounting.mostRecentReport); // <=== use get(getter)
-accounting.addEmployee(`Max`);
-accounting.addEmployee(`JovanAdmin`);
-accounting.addReport('Smthing went wrong...');
-accounting.describe();
-// accounting.printReports()
-// accounting.printEmployeeInformation()
+};
+user1.greet('Hi there I-am');
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,130 +1,18 @@
-abstract class Department { 
-//   private readyonly id: string;
-//   priovate name : string;
-//   private is only accessible from inside the class not from outside
-// but if we still want to be private but still accessible from the other classes instead of PRIVATE we use PROTECTED
-  static fiscalYear = 2020      
+interface Person {
+      name: string;
+      age : number;
 
-  protected employeess: string[] = [];
-
-    constructor(protected readonly id: string, public name: string )  { //<-- this is a shortcut from the previously written code
-      // this.id = id;
-      // this.name = n;
-      // console.log(Department.fiscalYear) <== This is not accessible from the constructor or the newlycreate instances
-      // but there is a way if we want to acces it by calling the name of the Class in this case like (Department.fiscalYear)
-      }
-
-    // creating a blueprint of this method just a structure so it can be used in the other classes that extends this class
-    abstract describe(this : Department): void 
-
-    static createEmployee( name: string ) {
-          return { name : name }; 
-    }
-
-    addEmployee(employee: string) {
-          this.employeess.push(employee);
-    }
-
-    printEmployeeInformation() {
-          console.log(this.employeess.length);
-          console.log(this.employeess);
-    }
+      greet(phrase: string) : void;
 }
 
-//even though the class is empty with the EXTENDS keyword we are gonna inherit everythig that class has
-// but when we want to get particular stuff we do it through the constructor and also by calling super() 
-class ITDepartment extends Department {
-      // this is passed to super
-      constructor( id: string, public admins : string[] ) {
-        super(id, 'IT');
-            this.admins = admins;
+let user1: Person;
+
+user1 = {
+      name : 'Max',
+      age  : 30,
+      greet(phrase: string) {
+            console.log(phrase + ' ' + this.name);
       }
+};
 
-      describe() {
-            console.log(`IT DEPARTMENT - ID ${this.id}`)
-      }
-}
-
-class AccountingDepartment extends Department {
-      private lastReport: string;
-
-      // this is used if we want to get a property of the class encapsulate it and add extra logic to it
-      // in this case we take the private lastReport and we want to add extra logic whenever we acces it
-      get mostRecentReport() {
-
-            if(this.lastReport) {
-                  return this.lastReport;
-            }
-            throw new Error(`No report found.`);
-
-      }
-
-      // with this we do the same but instead of getting a property we can set a new property encapsulate it and add extralogic to it
-     
-      set mostRecentReport(value: string) {
-
-            if(!value){
-                  throw new Error(`Plase pass in a valid value`);
-            }
-            this.addReport(value);
-
-      }
-
-      constructor( id: string, private reports: string[] ){
-
-            super(id, 'Accounting');
-            this.lastReport = reports[0];
-
-      }
-
-      
-      describe() {
-            console.log(`IT ACCOUNTING DEPARTNEBT - ID ${this.id}`)
-      }
-
-      addEmployee(name : string) {
-
-            if(name === 'Max') {
-                  return;
-            }
-            this.employeess.push(name);
-
-      }
-
-      addReport( text: string) {
-
-            this.reports.push(text);
-            this.lastReport = text;
-
-      }
-
-      printReports() {
-
-            console.log(this.reports);
-
-      }
-}
-
-const employee1 = Department.createEmployee('joko')
-console.log(employee1, Department.fiscalYear)
-const it = new ITDepartment('d1', ['Jojo']) 
-it.addEmployee('Max')
-it.addEmployee('Manu')
-
-it.describe()
-it.name = 'NEW NAME'
-it.printEmployeeInformation();
-console.log(it)
-
-
-const accounting = new AccountingDepartment('d2', []);
-accounting.mostRecentReport = `Your end report please`   // <=== use set(setter)
-console.log(accounting.mostRecentReport)                 // <=== use get(getter)
-
-accounting.addEmployee(`Max`);
-accounting.addEmployee(`JovanAdmin`);
-
-accounting.addReport('Smthing went wrong...');
-accounting.describe()
-// accounting.printReports()
-// accounting.printEmployeeInformation()
+user1.greet('Hi there I-am')
